f2874c78a15963cc0b067c25039c4847bd10efb5
manasdesai <62495882+manasdesai@users.noreply.github.com>
2021-09-01 23:39:04 +0530

    Update Ang_vel_service.py

--- week2/scripts/Ang_vel_service.py
+++ week2/scripts/Ang_vel_service.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
+from __future__ import print_function
+from std_msgs.msg import *
+from geometry_msgs.msg import *
 
+import sys
 import rospy
-from week2.srv import velocity, velocityResponse
-
-
-def handle_rad_req(req):
-    return velocityResponse(1/req.radius)
-
-
-def return_rad():
-    rospy.init_node('ang_vel_service_node')
-    s = rospy.Service('compute_ang_vel', velocity, handle_rad_req)
-    rospy.loginfo('Available for computing Angular Velocity')
+from ros_automation.srv import Ang_vel,Ang_velResponse
+def handle_Ang_vel(req):
+    resp=Ang_velResponse(0.1/req.radius)
+    print(resp)
+    return resp
+def return_Ang_vel_server():
+    rospy.init_node('compute_ang_vel',anonymous=True)
+    server=rospy.Service('compute_ang_vel',Ang_vel,handle_Ang_vel)
+    print('Returning the angular velocity')
     rospy.spin()
-
-if __name__ == "__main__":
-    return_rad()
+if __name__=='__main__':
+    return_Ang_vel_server()
